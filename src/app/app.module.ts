018ab2eac5104602e1b26ab17108e91022fc83be
Israel Vanderpuije <vanderpuijeisrael7@yahoo.com>
2022-01-18 18:47:08 +0000

    added table component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { ButtonComponent } from './components/button/button.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
@@ -14,8 +12,6 @@ import { TableComponent } from './table/table.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    ButtonComponent,
     NavbarComponent,
     TableComponent
   ],
